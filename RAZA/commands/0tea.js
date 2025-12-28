72974a84e63b314bf307601fc86ca2e9c559c1ca
Sksameersk86 <146408638+Sksameersk86@users.noreply.github.com>
2025-12-28 07:48:12 +0530

    Rename command from 'tea' to 'chai' and update response

--- RAZA/commands/0tea.js
+++ RAZA/commands/0tea.js
@@ -2,7 +2,7 @@ const { formatMessage } = require('../../utils/formatter');
 
 const fs = require("fs");
 module.exports.config = {
-  name: "tea",
+  name: "chai",
     version: "1.0.1",
   hasPermssion: 0,
   credits: "Kashif Raza", 
@@ -14,13 +14,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("tea")==0 || event.body.indexOf("Tea")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("CHAI")==0) {
+  if (event.body.indexOf("Chay")==0 || event.body.indexOf("chai")==0 || event.body.indexOf("Chai")==0 || event.body.indexOf("CHAI")==0) {
     var msg = {
-        body: "Ye Lo Bby ☕",
+        body: "PAKAR CHAI",
         attachment: fs.createReadStream(__dirname + `/noprefix/tea.mp4`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🫖", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🧸", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
